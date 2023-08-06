acb8b99534067ed714df8ddadc3fc13c27f2689b
Shalenikoff <KShalen@gmail.com>
2023-08-06 21:54:09 +0300

    Made model with names and surnames

--- ContactListApp/Models/Person.swift
+++ ContactListApp/Models/Person.swift
@@ -8,3 +8,20 @@
 import Foundation
 
 
+struct Person {
+
+    static func getPersonsFullName(from name: String, and surname: String ) -> String {
+        
+        let dataSource = DataManager()
+        
+        var name: String = dataSource.names.randomElement()!
+        var surname: String = dataSource.surnames.randomElement()!
+        var telephoneNumber: String = dataSource.phoneNumbers.randomElement()!
+        var email: String = dataSource.emails.randomElement()!
+        
+        let fullName = "\(name) \(surname))"
+        
+        return fullName
+    }
+    
+}
